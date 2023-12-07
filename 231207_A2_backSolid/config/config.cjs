100dcdaa95fd17fcd036215b2451b6294cbcf90e
nemo4 <7942apdkfl@hanmail.net>
2023-12-07 11:35:21 +0900

    port

--- 231207_A2_backSolid/config/config.cjs
+++ 231207_A2_backSolid/config/config.cjs
@@ -4,6 +4,7 @@ const development = {
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
   host: process.env.MYSQL_HOST,
   dialect: 'mysql',
 };
