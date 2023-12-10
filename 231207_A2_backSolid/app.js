b730f45d22202b030e8e3eef95994fb835b6e5ec
nemo4 <7942apdkfl@hanmail.net>
2023-12-11 08:47:57 +0900

    prisma

---
+++ 231207_A2_backSolid/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import PostsRouter from './routes/posts.router.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/api', [PostsRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
